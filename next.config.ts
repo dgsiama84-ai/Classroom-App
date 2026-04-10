c2ea562f01366bb5090f5208cd085d99c7c9af29
dgsiama84-ai <dgsiama84@gmail.com>
2026-04-10 21:53:34 +0800

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from 'next'
+import withPWA from '@ducanh2912/next-pwa'
 
 const nextConfig: NextConfig = {}
 
-// @ts-ignore
-const withPWA = require('next-pwa')({
+export default withPWA({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-})
-
-export default withPWA(nextConfig)
+})(nextConfig)
